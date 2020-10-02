20c2892f18d7554650ff5739ff36deaf14b9e130
Gimitlow <gleb.prischepa.mail.ru@gmail.com>
2020-10-03 01:24:40 +0300

    typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array) {
+    return array.length ? Math.min(...array) : 0;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array) {
+    return array.length ? Math.max(...array) : 0;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array) {
+    return array.length ? array.reduce((a, b) => (a + b)) / array.length : 0;
+  } else {
+    return 0;
+  }
 }
